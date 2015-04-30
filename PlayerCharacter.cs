9fc758f54dd9fbd639101d66f5bb04a29f28a2ab
mabdulra <mabdulra@gwmail.gwu.edu>
2015-04-30 11:33:43 -0400

    Expanded Player Class
    Added JRPG attribute stats to Player class.

--- PlayerCharacter.cs
+++ PlayerCharacter.cs
@@ -4,25 +4,57 @@ using System.Collections;
 [System.Serializable]
 public class PlayerCharacter
 {
-	public string name;
-	public int experience;
-	public int level;
-	public int nextLevel;
+	//	Player character constants
+	public static int WARRIOR	= 1;
+	public static int MAGELINE	= 2;
+	public static int DARKMAGE	= 3;
+	public static int HEALER	= 4;
 
-	public PlayerCharacter()
-	{
-		experience = 0;
-		level = 1;
-		nextLevel = 60;
-	}
+	//	Attributes of player characters
+	public int id;
+	public int hp;
+	public int atk;
+	public int def;
+	public int spd;
+	public int mAtk;
+	public int mDef;
+	public int costume;
 
-	public void GainLevel()
+	public PlayerCharacter(id)
 	{
-		if( experience >= nextLevel )
+		this.id = id;
+		int[] stats;
+
+		//	Set initial stats specifically for each character
+		switch(this.id)
 		{
-			experience -= nextLevel;
-			nextLevel *= 2;
-			level += 1;
+			case WARRIOR:
+				stats = new int[]{50,30,30,30,0,15};
+				break;
+			case MAGELING:
+				stats = new int[]{30,10,10,10,5,10};
+				break;
+			case DARKMAGE:
+				stats = new int[]{30,20,10,20,10,10};
+				break;
+			case HEALER:
+				stats = new int[]{70,50,20,25,5,10};
+				break;
+			default:
+				Debug.Log("UNSUPPORTED ID"+this.id+"PASSED TO PlayerCharacter CONSTRUCTOR");
+				stats = new int[]{0,0,0,0,0,0};
+				break;
 		}
+
+		//	Push stats from array to attributes
+		this.hp		= stats[0];
+		this.atk	= stats[1];
+		this.def	= stats[2];
+		this.spd	= stats[3];
+		this.mAtk	= stats[4];
+		this.mDef	= stats[5];
+
+		//	Until costumes are implemented fully, set them to default 0
+		this.costume = 0;
 	}
 }
