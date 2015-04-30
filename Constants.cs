ed72aba1d0332e40324efeb47088806fc7e375b7
mabdulra <mabdulra@gwmail.gwu.edu>
2015-04-30 14:10:19 -0400

    Addition of Constants class

---
+++ Constants.cs
@@ -0,0 +1,27 @@
+/**
+ *  This class contains Constants used in serialization
+ *  The constants are not included in the serialized objects
+ *  We use the short name `C` for simplicity purposes
+ */
+public static class C
+{
+    //	Player character constants
+	public const int WARRIOR	= 1;
+	public const int MAGELINE	= 2;
+	public const int DARKMAGE	= 3;
+	public const int HEALER		= 4;
+
+    //  Player costume constants
+    public const int COSTUME_DEFAULT    = 1;
+    public const int COSTUME_SANTA      = 2;
+    public const int COSTUME_FORMAL     = 3;
+
+    //	Item id constants
+    public const int POTION         = 1;
+
+    //	Item type constants
+    public const int ITEM_HEALING   = 1;
+    public const int ITEM_EQUIP     = 2;
+    public const int ITEM_KEY       = 3;
+
+}
