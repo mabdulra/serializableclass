719b8c2d95913acb864bfb0bbea4158b769a2826
mabdulra <mabdulra@gwmail.gwu.edu>
2015-04-30 08:54:40 -0400

    Initial push
    SaveLoad uses BinaryFormatter to write/read to/from a FileStream containing our binary saved game file. Unity saves the game in its persistent data path.

---
+++ SaveLoad.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class SaveLoad
+{
+	private const string SAVEPATH = "/savedGame.bin";
+
+	public static void Save(Game savedGame)
+	{
+		Debug.Log("START OF SAVE GAME PROCESS");
+
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath+SAVEPATH);
+		bf.Serialize(file, savedGame);
+		file.Close();
+
+		Debug.Log("END OF SAVE GAME PROCESS");
+	}
+
+	public static Game Load()
+	{
+		Debug.Log("START OF LOAD GAME PROCESS");
+		Debug.Log(Application.persistentDataPath + SAVEPATH);
+
+		if( File.Exists(Application.persistentDataPath+SAVEPATH) )
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath+SAVEPATH, FileMode.Open);
+			Game savedGame = (Game)bf.Deserialize(file);
+			file.Close();
+			Debug.Log("END OF LOAD GAME PROCESS");
+			return savedGame;
+		}
+
+		Debug.Log("END OF LOAD GAME PROCESS (NO SAVE GAME FOUND)");
+		return null;
+	}
+}
